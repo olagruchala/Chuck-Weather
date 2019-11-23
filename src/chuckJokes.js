34429b8cbb02b49b7ca89d66b52b2173f291be3e
Ola <aleksandra@gruchala.eu>
2019-11-23 18:52:05 +0100

    Add file with Chuck jokes

---
+++ src/chuckJokes.js
@@ -0,0 +1,7 @@
+const jokes = [
+  ["Chuck Norris nie zmiata kurzy, to kurze zmiatają przed nim."],
+  ["Każdy wie, że Chuck Norris doliczył do nieskończoności dwa razy. Ale nie każdy wie, że zaczynał od minus nieskończoności."],
+  ["Chuck Norris nie ma cienia."]
+];
+
+export default jokes;
