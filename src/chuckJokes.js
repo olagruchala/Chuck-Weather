4de587febb8870f497085f76286cda9ba5172811
kontaktzajac <kontakt.zajac@gmail.com>
2019-11-25 11:40:25 +0100

    Add API chuck norris jokes

--- src/chuckJokes.js
+++ src/chuckJokes.js
@@ -3,10 +3,18 @@ async function jokes(){
   await fetch("src/jokes.json")
   .then((res) => res.json())
   .then((data) => {
-  const indexTable = Math.floor(Math.random() *data.jokes.length);
-  joke = data.jokes[indexTable].joke;
+    const indexTable = Math.floor(Math.random() *data.jokes.length);
+    joke = data.jokes[indexTable].joke;
   })
+  jokesAPI();
   return joke;
-  }
+}
 
-export default jokes;
+async function jokesAPI(name)
+{
+  const response = await fetch(`https://api.chucknorris.io/jokes/random?name=${name}`);
+  let joke = await response.json();
+  return joke.value;
+}
+
+export {jokes, jokesAPI};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-import jokes from "./chuckJokes.js";
+import {jokes, jokesAPI} from "./chuckJokes.js";
 
 jokes().then((res) => { console.log("Żart chuck`a, plik index.js: "+res); /* tutaj łapiemy element div, w którym mają być żarty i wpisujemy wartość res */ });
-
+jokesAPI("UserName").then((res) => { console.log("Żart chuck`a, zewnętrzne API: "+res); /* tutaj łapiemy element div, w którym mają być żarty i wpisujemy wartość res */ });
