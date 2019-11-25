e75962d42a7861b39bd899fcb8d41a7b79ef847a
Ola <aleksandra@gruchala.eu>
2019-11-25 19:16:05 +0100

    Take matching weather elements

--- src/current.js
+++ src/current.js
@@ -1,17 +1,25 @@
 
 const apiKey = "c9c2007393ea74869a3499e9027762f6";
+let city = "Thessaloniki"; //input.value -> toLowercase
+// Thessaloniki - city with rain
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-
-// Class for the current weather
-export default class Current {
-
-  getWeatherByCityName(city) {
-    fetch(`api.openweathermap.org/data/2.5/weather?q=${city}forecast?id=524901&APPID=${apiKey}`)
-      .then(response => {
-        return response.json();
-       })
-      .then(data => console.log(data))
-  }
-}
-
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    const { pressure, temp, humidity } = data.main;
+    const { speed, deg } = data.wind;
+    const { description } = data.weather[0];
+    const rain = data.rain['1h'];
+    // const snow = data.snow['1h'];
+    console.log(pressure);
+    console.log(temp);
+    console.log(humidity);
+    console.log(speed);
+    console.log(deg);
+    console.log(description);
+    console.log(rain);
+    // console.log(snow);
+  });
 
